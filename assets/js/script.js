965893355d28d430850dd7b3a0f2fb5072bdcd64
durbi dakka <dur2dakka@gmail.com>
2020-04-12 20:14:26 +0300

    1

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,24 @@ $('input.nav-checks').on('change', function() {
     $('input.nav-checks').not(this).prop('checked', false);  
 });
 
+
+/* 
+$(document).on("scroll", function() {
+
+	if($(document).scrollTop()>100) {
+		$("#nav-bar").removeClass("large").addClass("small");
+	} else {
+		$("#nav-bar").removeClass("small").addClass("large");
+	}
+	
+}); */
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    document.getElementById("nav-bar").style.backgroundColor = "red";
+  } else {
+    document.getElementById("nav-bar").style.backgroundColor = "gray";
+  };
+};
