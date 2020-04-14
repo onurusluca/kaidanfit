eae18f7b246188a18fbaa9c2f04ec2bcd592b335
durbi dakka <dur2dakka@gmail.com>
2020-04-15 00:53:18 +0300

    new

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,3 +22,42 @@ btn.on('click', function(e) {
   $('html, body').animate({scrollTop:0}, '300');
 });
 
+
+// Hide Header on on scroll down
+var didScroll;
+var lastScrollTop = 0;
+var delta = 5;
+var navbarHeight = $('#bottom-tab-bar').outerHeight();
+
+$(window).scroll(function(event){
+    didScroll = true;
+});
+
+setInterval(function() {
+    if (didScroll) {
+        hasScrolled();
+        didScroll = false;
+    }
+}, 250);
+
+function hasScrolled() {
+    var st = $(this).scrollTop();
+    
+    // Make sure they scroll more than delta
+    if(Math.abs(lastScrollTop - st) <= delta)
+        return;
+    
+    // If they scrolled down and are past the navbar, add class .nav-up.
+    // This is necessary so you never see what is "behind" the navbar.
+    if (st > lastScrollTop && st > navbarHeight){
+        // Scroll Down
+        $('#bottom-tab-bar').removeClass('tab-bar-toggle').addClass('hide-tab-bar');
+    } else {
+        // Scroll Up
+        if(st + $(window).height() < $(document).height()) {
+            $('#bottom-tab-bar').removeClass('hide-tab-bar').addClass('tab-bar-toggle');
+        }
+    }
+    
+    lastScrollTop = st;
+}
